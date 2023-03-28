39dca1b45fa6ef81ff659fdc20ece8fa257fa1f1
Peter Garner <garner.pd@pg.com>
2023-03-28 00:23:19 -0400

    back to google

--- instrumentation-node.ts
+++ instrumentation-node.ts
@@ -8,7 +8,7 @@ import {
 // You can use gRPC exporter instead
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 
-//import { TraceExporter } from "@google-cloud/opentelemetry-cloud-trace-exporter";
+import { TraceExporter } from "@google-cloud/opentelemetry-cloud-trace-exporter";
 
 // Next.js expects you to use to register TraceProvider. It won't work if you use NodeSDK.
 // We use registered provider to create traces inside of Next.js internals.
@@ -20,8 +20,8 @@ const provider = new NodeTracerProvider({
 
 // Initialize the exporter. When your application is running on Google Cloud,
 // you don't need to provide auth credentials or a project id.
-//const exporter = new TraceExporter();
-const exporter = new OTLPTraceExporter();
+const exporter = new TraceExporter();
+//const exporter = new OTLPTraceExporter();
 
 //provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter({})))
 
