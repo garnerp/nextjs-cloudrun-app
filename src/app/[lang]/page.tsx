51860a8eda4b19639796b6b595b8a36607f578bb
Peter Garner <garner.pd@pg.com>
2023-03-28 23:21:16 -0400

    lang middleware

---
+++ src/app/[lang]/page.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { fetchGithubStars } from '../fetchGithubStars'
+import { Locale } from '../i18n-config'
+
+export default async function Home({
+  params: { lang },
+}: {
+  params: { lang: Locale }
+}) {
+  const stars = await fetchGithubStars()
+
+  return (
+    <>
+      <div>The Index Page</div>
+      <p>Next.js has {stars} ⭐️</p>
+      <p>Living on language {lang}</p>
+    </>
+  )
+}
