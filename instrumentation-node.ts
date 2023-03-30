6dc2e75fa599299e56186c5977dce9f2079827c1
Peter Garner <garner.pd@pg.com>
2023-03-29 21:48:20 -0400

    testing new otel register

--- instrumentation-node.ts
+++ instrumentation-node.ts
@@ -13,21 +13,20 @@ import { TraceExporter } from "@google-cloud/opentelemetry-cloud-trace-exporter"
 
 // Next.js expects you to use to register TraceProvider. It won't work if you use NodeSDK.
 // We use registered provider to create traces inside of Next.js internals.
-const provider = new NodeTracerProvider({
-    sampler: new AlwaysOnSampler(),
-    resource: new Resource({
-        [SemanticResourceAttributes.SERVICE_NAME]: 'garner-next-app',
-    }),
-})
 
-// Initialize the exporter. When your application is running on Google Cloud,
-// you don't need to provide auth credentials or a project id.
-//const exporter = new TraceExporter();
-//const exporter = new OTLPTraceExporter();
+export function register() {
+    // Next.js expects you to use to register TraceProvider. It won't work if you use NodeSDK.
+    // We use registered provider to create traces inside of Next.js internals.
 
-//provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter({})))
-//provider.addSpanProcessor(new SimpleSpanProcessor(exporter))
-provider.addSpanProcessor(new BatchSpanProcessor(new TraceExporter()));
+    const provider = new NodeTracerProvider({
+        sampler: new AlwaysOnSampler(),
+        resource: new Resource({
+            [SemanticResourceAttributes.SERVICE_NAME]: 'g-next-app',
+        }),
+    })
 
-// Make sure to register you provider
-provider.register()
+    provider.addSpanProcessor(new BatchSpanProcessor(new TraceExporter()));
+
+    // Make sure to register you provider
+    provider.register()
+}
