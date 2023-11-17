ea37afc6db2119072f7018886f5dde29a48e21d4
Peter Garner <garnerp@gmail.com>
2023-11-17 12:04:10 -0500

    fun with next auth and Google Provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,34 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { fetchGithubStars } from './utils/fetchGithubStars'
-import logger from '../app/utils/logger'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { fetchGithubStars } from "./utils/fetchGithubStars";
+import logger from "../app/utils/logger";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/lib/auth";
+import CC from "./cc";
 
 export default async function Home() {
-
-  //  logger.info("Info from page")
-
-  const stars = await fetchGithubStars()
+  const stars = await fetchGithubStars();
+  const sess = await getServerSession(authOptions);
+  console.log(sess);
 
   return (
     <>
       <div className="text-3xl">The Index Page</div>
       <p>Next.js has {stars} ⭐️</p>
-      <p>I wonder how fast this comes up?  Takes about .4 seconds.  Timing off turbo?</p>
-      <p>Did this recompile?  Now did it work?</p>
+      <p>
+        I wonder how fast this comes up? Takes about .4 seconds. Timing off
+        turbo?
+      </p>
+      <p>Did this recompile? Now did it work?</p>
       <button>Hi</button>
       <button>This is #2</button>
-      Did this work?  Yes it did.  Test.
-      Next v14 on Turbo!
-      Is it fast?
+      Did this work? Yes it did. Test. Next v14 on Turbo! Is it fast?
+      <br />
+      Expires: {sess?.expires}
+      <br />
+      User: {sess?.user?.email}
+      <br />
+      <CC />
     </>
-  )
+  );
 }
--- src/app/providers.js
+++ src/app/providers.js
@@ -1,7 +1,6 @@
-'use client';
+"use client";
+import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }) {
-    return (
-        <><div>Provider Component</div>{children}</>
-    );
+  return <SessionProvider>{children}</SessionProvider>;
 }
